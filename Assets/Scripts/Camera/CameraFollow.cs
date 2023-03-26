ff2f8c5bff61adbf7a51e188f7f3676bbb2011f2
Sleepyss <sleepyneko23@gmail.com>
2023-03-26 21:36:04 +0700

    added CameraFollow Function

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform player;
+    [SerializeField] private float offsetY;
+
+    [SerializeField] private float FollowSpeed = 2f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = new Vector3(player.position.x, player.position.y + offsetY, -10f);
+        transform.position = Vector3.Slerp(transform.position, pos, FollowSpeed * Time.deltaTime);
     }
 }
